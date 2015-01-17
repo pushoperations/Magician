348cc8ad284d79ea957f115ee7ff1d6f13a59c50
Tony Li <tonyli.lives@gmail.com>
2015-01-16 18:00:31 -0800

    docs generator

---
+++ docs.php
@@ -0,0 +1,24 @@
+<?php require __DIR__ . '/vendor/autoload.php';
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in($dir = __DIR__ . '/src');
+
+$versions = GitVersionCollection::create($dir)
+    ->add('master', 'master branch')
+    ->addFromTags('v*.*');
+
+$options = array(
+    'versions'             => $versions,
+    'title'                => 'Magician API',
+    'build_dir'            => __DIR__ . '/build/docs/%version%',
+    'cache_dir'            => __DIR__ . '/build/cache/docs/%version%',
+    'default_opened_level' => 2,
+);
+
+return new Sami($iterator, $options);
