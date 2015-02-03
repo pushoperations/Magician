8f29374d5f5d1c14856de85e70636b785d2bad67
Tony Li <tonyli.lives@gmail.com>
2015-02-03 14:06:54 -0800

    cache independently
    give each magician instance that is managing a specific model a separate cache

--- src/Magician.php
+++ src/Magician.php
@@ -12,6 +12,19 @@ class Magician extends Repository
     protected $cacheTag = 'magician-repository';
 
     /**
+     * Create a new instance of the Magician repository.
+     *
+     * @param  string $name The namespaced name of the model the manager will be managing
+     * @return void
+     */
+    public function __construct($name = null)
+    {
+        if ($name) {
+            $this->set($name);
+        }
+    }
+
+    /**
      * Set the managed model.
      *
      * @param  string $name The namespaced name of the model the manager will be managing
@@ -20,7 +33,19 @@ class Magician extends Repository
     public function set($name)
     {
         $this->model = App::make($name);
+        $this->cacheTag = $this->tagify($name).'-magician-repository';
 
         return $this;
+    }
+
+    /**
+     * Generate a formatted string from a given namespaced string.
+     *
+     * @param  string $string
+     * @return string
+     */
+    private function tagify($string)
+    {
+        return strtolower(preg_replace('/\\\/', '-', $string));
     }
 }
